6b50e26e681ab7cda8e062c65ac665bf9f2e9d73
wangteng <wangteng@geekplus.com.cn>
2019-02-22 16:16:05 +0800

    代码结构调整及集成dubbo

--- demo-service/src/main/java/com/demo/service/Impl/UserServiceImpl.java
+++ demo-service/src/main/java/com/demo/service/Impl/UserServiceImpl.java
@@ -14,8 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
 
 /**
  * Author: wt
--- demo-task/src/main/java/com/demo/TaskStartApplication.java
+++ demo-task/src/main/java/com/demo/TaskStartApplication.java
@@ -1,11 +1,13 @@
 package com.demo;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.demo.dao")
+@EnableDubbo
 public class TaskStartApplication {
 
     public static void main(String[] args) {
